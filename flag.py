7630945119baf074f05b404cba97b437117d435d
Sergey <serg015191@gmail.com>
2023-09-06 21:46:40 +0300

    Changes

---
+++ flag.py
@@ -0,0 +1,11 @@
+
+import turtle
+
+i = 0
+turtle.shape('classic')
+while (i < 73):
+    turtle.forward(15)
+    turtle.left(5)
+    i += 1
+
+turtle.done()
--- pyproga.py
+++ pyproga.py
@@ -1,16 +1,18 @@
-x = 1234567890
+x = 1237178637
 a = [k for k in range(0, 10)]
 b = []
 
 while (len(b) != 10):
     b.append(int(0))
 
-while (((x // 10) != 0) or ((x >= 1) & (x <= 9))):
+while (((x // 10) != 0) or ((x >= 0) & (x <= 9))):
     for i in a:
         if (x % 10) == i:
             b[i] += 1
-            x = x // 10
-
+            x =  x // 10
+    if x == 0:
+        break
+            
 print("Количество цифр в числе:")
 for i in a:
     print(a[i], "---", b[i])
